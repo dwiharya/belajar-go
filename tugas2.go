2ff026a341cf3e6e40687607eb9d62628b92f46a
dwiharya <dwih02@gmail.com>
2023-06-12 21:05:57 +0700

    tugas 2 fix

--- tugas2.go
+++ tugas2.go
@@ -3,11 +3,17 @@ import "fmt"
 
 func main() {
 	for i :=0; i<16; i++{
+		var f = "Fizz"
+		var b ="Buzz"
 		if i % 3 == 0 {
-			fmt.Println("Fizz")
+			fmt.Println(f)
 		 }
 		 if i % 5 == 0 {
-			fmt.Println("Buzz")
+			fmt.Println(b)
+		 }
+
+		 if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println(f+b)
 		 }
 		 
 		 if i % 3 > 0 || i % 5 > 0 {
